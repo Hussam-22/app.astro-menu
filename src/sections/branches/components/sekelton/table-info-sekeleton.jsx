46483b9656cea08de5cd05f4144cb001c84dfa83
Hussam-22 <hussam@hotmail.co.uk>
2024-01-17 15:35:38 +0400

    fix

--- src/sections/branches/components/sekelton/table-info-sekeleton.jsx
+++ src/sections/branches/components/sekelton/table-info-sekeleton.jsx
@@ -3,14 +3,14 @@ import Grid from '@mui/material/Unstable_Grid2/Grid2';
 
 function TableInfoSkeleton() {
   return (
-    <Grid container spacing={2}>
+    <>
       <Grid sm={8}>
-        <Skeleton variant="rounded" animation="wave" sx={{ width: '100%', height: 400 }} />
+        <Skeleton variant="rounded" animation="wave" sx={{ height: 365 }} />
       </Grid>
       <Grid sm={4}>
-        <Skeleton variant="rounded" animation="wave" sx={{ width: '100%', height: 400 }} />
+        <Skeleton variant="rounded" animation="wave" sx={{ height: 365 }} />
       </Grid>
-    </Grid>
+    </>
   );
 }
 export default TableInfoSkeleton;
