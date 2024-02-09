a0da399797d922e7e8b275419a509ef334cc0f08
Hussam-22 <hussam@hotmail.co.uk>
2024-02-10 02:07:55 +0400

    update

--- src/sections/qr-menu/meal-card.jsx
+++ src/sections/qr-menu/meal-card.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import { useQueryClient } from '@tanstack/react-query';
 
-import { Card, Stack, Typography } from '@mui/material';
+import { Box, Card, Stack, Typography } from '@mui/material';
 
 import Image from 'src/components/image';
+import Label from 'src/components/label';
 
 function MealCard({ mealInfo }) {
   //   const { lang } = useParams();
@@ -12,16 +13,37 @@ function MealCard({ mealInfo }) {
     mealInfo;
 
   const queryClient = useQueryClient();
-  const cachedData = queryClient.getQueryData(['mealsLabel', userID]);
+  const cachedMealLabels = queryClient.getQueryData(['mealsLabel', userID]);
 
-  console.log(cachedData);
+  const labels = cachedMealLabels.filter((cachedMealLabel) =>
+    mealLabels.includes(cachedMealLabel.docID)
+  );
 
   return (
     <Card sx={{ bgcolor: 'background.default', p: 3 }}>
       <Stack direction="column" spacing={1}>
-        <Image src={cover} ratio="16/9" sx={{ borderRadius: 2 }} />
+        <Box sx={{ position: 'relative' }}>
+          <Image src={cover} ratio="16/9" sx={{ borderRadius: 1 }} />
+          {isNew && (
+            <Box sx={{ position: 'absolute', bottom: -15, right: 10 }}>
+              <Label
+                variant="filled"
+                color="error"
+                sx={{ fontSize: 20, p: 2, boxShadow: '5px 5px 0 0 #000' }}
+              >
+                New
+              </Label>
+            </Box>
+          )}
+        </Box>
+
         <Typography variant="h4">{title}</Typography>
         <Typography variant="body2">{description}</Typography>
+        <Stack direction="row" spacing={1}>
+          {labels.map((label) => (
+            <Label variant="filled">#{label.title}</Label>
+          ))}
+        </Stack>
       </Stack>
     </Card>
   );
