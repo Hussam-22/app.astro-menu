f3068b891fb808e9621a3b79b2fc61bf215d8c96
Hussam-22 <hussam@hotmail.co.uk>
2024-06-17 17:16:40 +0400

    add tailwind rose color

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -71,11 +71,26 @@ const ERROR = {
   contrastText: '#FFFFFF',
 };
 
+const ROSE = {
+  50: '#fff1f2',
+  100: '#ffe4e6',
+  200: '#fecdd3',
+  300: '#fda4af',
+  400: '#fb7185',
+  500: '#f43f5e',
+  600: '#e11d48',
+  700: '#be123c',
+  800: '#9f1239',
+  900: '#881337',
+  contrastText: '#FFFFFF',
+};
+
 const COMMON = {
   common: {
     black: '#000000',
     white: '#FFFFFF',
   },
+  rose: ROSE,
   primary: PRIMARY,
   secondary: SECONDARY,
   info: INFO,
