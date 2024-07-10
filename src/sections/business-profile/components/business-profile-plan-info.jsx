34c1b1339a748b81b4bcfa44236e0cf88cdb6e46
Hussam-22 <hussam@hotmail.co.uk>
2024-07-11 00:06:49 +0400

    update

--- src/sections/business-profile/components/business-profile-plan-info.jsx
+++ src/sections/business-profile/components/business-profile-plan-info.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { useMutation } from '@tanstack/react-query';
 
 import { LoadingButton } from '@mui/lab';
-import { Card, Stack, Divider, Typography } from '@mui/material';
+import { Box, Card, Stack, Divider, Typography } from '@mui/material';
 
 import Label from 'src/components/label';
 import { STRIPE } from 'src/config-global';
@@ -18,7 +18,6 @@ import { fDate, fDistance } from 'src/utils/format-time';
 
 function BusinessProfilePlanInfo() {
   const { businessProfile } = useAuthContext();
-
   const {
     media,
     name,
@@ -74,7 +73,7 @@ function BusinessProfilePlanInfo() {
 
   const getPortalSessionUrl = async () => {
     const body = {
-      customerEmail: 'hussam@hotmail.co.uka',
+      customerEmail: 'hussam@hotmail.co.uk',
     };
     const headers = {
       'Content-Type': 'application/json',
@@ -85,7 +84,7 @@ function BusinessProfilePlanInfo() {
     // http://localhost:4242/create-checkout-session
     // gebp-dgbh-dfpy-dsor-szfs
 
-    const response = await fetch('http://localhost:4242/create-portal-session', {
+    const response = await fetch('https://stripe-astro-menu.vercel.app/create-portal-session', {
       method: 'POST',
       headers,
       body: JSON.stringify(body),
@@ -93,114 +92,118 @@ function BusinessProfilePlanInfo() {
 
     const res = await response.json();
 
-    console.log(res);
-
-    // window.location = res.url;
+    window.location = res.url;
   };
 
   return (
-    <Card sx={{ p: 3, position: 'relative' }}>
-      <Stack
-        direction="row"
-        spacing={2}
-        justifyContent="space-between"
-        sx={{ position: 'absolute', top: 25, right: 25 }}
-      >
-        {isTrial && (
-          <Label color={isTrial ? 'warning' : 'primary'} sx={{ fontSize: 16, p: 2 }}>
-            {isTrial ? 'Trial' : 'Subscribed'}
-          </Label>
-        )}
-        <Label color={labelContent().color} sx={{ fontSize: 16, p: 2 }}>
-          {labelContent().label}
-        </Label>
-      </Stack>
-      <Stack
-        spacing={3}
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        divider={<Divider flexItem orientation="vertical" sx={{ borderStyle: 'dashed' }} />}
-      >
+    <>
+      <Card sx={{ p: 3, position: 'relative' }}>
         <Stack
-          direction="column"
+          direction="row"
           spacing={2}
-          justifyContent="center"
-          alignItems="center"
-          sx={{ maxWidth: '18%' }}
+          justifyContent="space-between"
+          sx={{ position: 'absolute', top: 25, right: 25 }}
         >
-          <Stack direction="column" spacing={0} justifyContent="center" alignItems="center">
-            <Typography variant="overline" sx={{ color: 'grey.400' }}>
-              current plan
-            </Typography>
-            <Typography variant="h4">{name}</Typography>
-            <Typography variant="overline">
-              {isTrial ? 'Free' : `${paymentInfo.amount}$ / ${paymentInfo.term}`}
-            </Typography>
-          </Stack>
-          <Image src={`/assets/icons/plans/${media.icon}.svg`} sx={{ borderRadius: 1, mx: 2 }} />
-          <LoadingButton
-            variant="contained"
-            color="primary"
-            startIcon={<Iconify icon="heroicons-solid:switch-vertical" />}
-            onClick={() => mutate(getCheckoutUrl)}
-            loading={isPending}
-          >
-            Change Plan
-          </LoadingButton>
-
-          <LoadingButton
-            variant="contained"
-            color="primary"
-            startIcon={<Iconify icon="heroicons-solid:switch-vertical" />}
-            onClick={() => mutate(getPortalSessionUrl)}
-            loading={isPending}
-          >
-            Manage Subscription
-          </LoadingButton>
-        </Stack>
-        <Stack direction="column" spacing={2} flexGrow={1}>
           {isTrial && (
-            <PlanInfoItem
-              title="Trial Expiration Date"
-              content={`${trialExpirationDate} | In ${remainingDays}`}
-            />
+            <Label color={isTrial ? 'warning' : 'primary'} sx={{ fontSize: 16, p: 2 }}>
+              {isTrial ? 'Trial' : 'Subscribed'}
+            </Label>
           )}
+          <Label color={labelContent().color} sx={{ fontSize: 16, p: 2 }}>
+            {labelContent().label}
+          </Label>
+        </Stack>
+        <Stack
+          spacing={3}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          divider={<Divider flexItem orientation="vertical" sx={{ borderStyle: 'dashed' }} />}
+        >
+          <Stack
+            direction="column"
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+            sx={{ maxWidth: '18%' }}
+          >
+            <Stack direction="column" spacing={0} justifyContent="center" alignItems="center">
+              <Typography variant="overline" sx={{ color: 'grey.400' }}>
+                current plan
+              </Typography>
+              <Typography variant="h4">{name}</Typography>
+              <Typography variant="overline">
+                {isTrial ? 'Free' : `${paymentInfo.amount}$ / ${paymentInfo.term}`}
+              </Typography>
+            </Stack>
+            <Image src={`/assets/icons/plans/${media.icon}.svg`} sx={{ borderRadius: 1, mx: 2 }} />
+          </Stack>
+          <Stack direction="column" spacing={2} flexGrow={1}>
+            {isTrial && (
+              <PlanInfoItem
+                title="Trial Expiration Date"
+                content={`${trialExpirationDate} | In ${remainingDays}`}
+              />
+            )}
 
-          <PlanInfoItem title="Description" content={description} />
-          <Divider sx={{ borderStyle: 'dashed' }} />
-          <Stack direction="row" spacing={2} justifyContent="space-evenly" flexWrap>
-            <FeatureItem
-              icon="lucide:scan-search"
-              value={scans === 0 ? 'Unlimited*' : scans}
-              title="Total Scans"
-            />
-            <FeatureItem
-              icon="heroicons:building-storefront"
-              value={`${branch}`}
-              title="Branches"
-            />
-            <FeatureItem icon="lucide:qr-code" value={`${tables} per branch`} title="QR Codes" />
-            <FeatureItem icon="hugeicons:translate" value={`${languages}`} title="Languages" />
-            <FeatureItem icon="mdi:network-pos" value={!!pos} title="POS Dashboard" />
-            {/* <FeatureItem
+            <PlanInfoItem title="Description" content={description} />
+            <Divider sx={{ borderStyle: 'dashed' }} />
+            <Stack direction="row" spacing={2} justifyContent="space-evenly" flexWrap>
+              <FeatureItem
+                icon="lucide:scan-search"
+                value={scans === 0 ? 'Unlimited*' : scans}
+                title="Total Scans"
+              />
+              <FeatureItem
+                icon="heroicons:building-storefront"
+                value={`${branch}`}
+                title="Branches"
+              />
+              <FeatureItem icon="lucide:qr-code" value={`${tables} per branch`} title="QR Codes" />
+              <FeatureItem icon="hugeicons:translate" value={`${languages}`} title="Languages" />
+              <FeatureItem icon="mdi:network-pos" value={!!pos} title="POS Dashboard" />
+              {/* <FeatureItem
               icon="heroicons:users"
               value={subUser > 1 ? `${subUser} users` : false}
               title="Sub Users"
             /> */}
-            <FeatureItem icon="hugeicons:analytics-up" value={!!analytics} title="Analytics" />
-            <FeatureItem
-              icon="hugeicons:shopping-basket-add-03"
-              value={!isMenuOnly}
-              title="Self Order"
-            />
+              <FeatureItem icon="hugeicons:analytics-up" value={!!analytics} title="Analytics" />
+              <FeatureItem
+                icon="hugeicons:shopping-basket-add-03"
+                value={!isMenuOnly}
+                title="Self Order"
+              />
+            </Stack>
+            <Typography variant="body2" sx={{ color: 'grey.600' }}>
+              * Unlimited scans are subject to fair use policy
+            </Typography>
           </Stack>
-          <Typography variant="body2" sx={{ color: 'grey.600' }}>
-            * Unlimited scans are subject to fair use policy
-          </Typography>
         </Stack>
-      </Stack>
-    </Card>
+      </Card>
+
+      <Card sx={{ mt: 2 }}>
+        <Stack direction="row" spacing={2} justifyContent="space-between" alignItems="center" p={3}>
+          <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+            Use the{' '}
+            <Box component="span" sx={{ color: 'primary.main' }}>
+              Subscription Portal
+            </Box>{' '}
+            to Upgrade/Downgrade your Plan, Download payment Invoices, Update your Payment Info or
+            Cancel your Subscription
+          </Typography>
+          <LoadingButton
+            variant="contained"
+            color="secondary"
+            startIcon={<Iconify icon="akar-icons:gear" />}
+            onClick={() => mutate(getPortalSessionUrl)}
+            loading={isPending}
+            sx={{ whiteSpace: 'nowrap', px: 5 }}
+          >
+            Open Subscription Portal
+          </LoadingButton>
+        </Stack>
+      </Card>
+    </>
   );
 }
 
