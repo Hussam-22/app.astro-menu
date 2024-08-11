87dfa95923fa883b28bf12265d48ca97edf2d32e
Hussam-22 <hussam@hotmail.co.uk>
2024-08-11 14:04:46 +0400

    feat: Update table skeleton to display fewer rows and adjust column width

--- src/components/table/table-skeleton.js
+++ src/components/table/table-skeleton.js
@@ -7,13 +7,12 @@ import TableCell from '@mui/material/TableCell';
 // ----------------------------------------------------------------------
 
 export default function TableSkeleton({ ...other }) {
-  return [...Array(10)].map((_, index) => (
+  return [...Array(3)].map((_, index) => (
     <TableRow {...other} key={index}>
-      <TableCell colSpan={12}>
+      <TableCell colSpan={5}>
         <Stack spacing={3} direction="row" alignItems="center">
-          <Skeleton variant="rounded" animation="wave" sx={{ width: '15%', height: 22 }} />
-          <Skeleton variant="rounded" animation="wave" sx={{ width: '15%', height: 12 }} />
-          <Skeleton variant="rounded" animation="wave" sx={{ width: '15%', height: 12 }} />
+          <Skeleton variant="rounded" animation="wave" sx={{ width: '35%', height: 22 }} />
+          <Skeleton variant="rounded" animation="wave" sx={{ width: '20%', height: 12 }} />
           <Skeleton variant="rounded" animation="wave" sx={{ width: '15%', height: 12 }} />
           <Skeleton variant="rounded" animation="wave" sx={{ width: '15%', height: 12 }} />
           <Skeleton variant="rounded" animation="wave" sx={{ width: '15%', height: 12 }} />
